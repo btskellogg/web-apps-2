7b096de7b507589f3f15aec22a58a3a026ef463f
Benjamin Block <bjblock@gmail.com>
2023-05-10 23:13:58 +0000

    comment out posts controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,17 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-    # render posts/new view with new Post form
-  end
+  # def new
+  #   @post = Post.new
+  #   # render posts/new view with new Post form
+  # end
 
-  def create
-    # start with a new Post
-    @post = Post.new
-    # assign user-entered form data to Post's columns
-    # save Post row
-    # redirect user
-  end
+  # def create
+  #   # start with a new Post
+  #   @post = Post.new
+  #   # assign user-entered form data to Post's columns
+  #   # save Post row
+  #   # redirect user
+  # end
   
 end
