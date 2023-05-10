ab13c826740ef608581d18620dafe10ac784c4c4
Benjamin Block <bjblock@gmail.com>
2023-05-10 22:32:24 +0000

    adds bitcoins resource and coindesk API

---
+++ app/controllers/bitcoins_controller.rb
@@ -0,0 +1,6 @@
+class BitcoinsController < ApplicationController
+  def index
+    # add backend code
+    # render bitcoins/index view
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources "tacos"
   resources "dice"
+  resources "bitcoins"
   resources "companies"
   resources "contacts"
   resources "posts"
